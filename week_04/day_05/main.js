9427adef34b08b8d36e75b34e46ee465441a5096
alanoud-GA <alanoud.alrasheed@generalassemb.ly>
2019-03-21 09:08:23 +0300

    w04d05

---
+++ week_04/day_05/main.js
@@ -0,0 +1,62 @@
+// Act I
+
+var hamlets = [
+    "Laurence Olivier, 1948",
+    "Grigori Kozintsev, 1964",
+    "Tony Richardson, 1969",
+    "Franco Zeffirelli, 1990",
+    "Kenneth Branagh, 1996",
+    "Michael Almereyda, 2000"
+];
+
+// Scene I
+// Use .forEach to print out "O! the venerable [Actor] starred as Hamlet in [Year]"
+console.log("Scene I ---------------------------");
+
+// Scene II
+//Programatically convert the hamlets array into an array of arrays where the
+//first element, name, is a string, and the second element, year, is an integer.
+console.log("Scene II ---------------------------");
+
+// Scene III
+// Programatically convert the hamlets array of arrays into an array of objects
+// where the keys are name and year
+console.log("Scene III ---------------------------");
+
+// Scene IV
+// Using the object of arrays from Scene III, programatically select all the Hamlets from the 1960's and assign them to an
+// array called swingin_hamlets
+console.log("Scene IV ---------------------------");
+
+// Scene V
+// // Using the object of arrays from Scene III, programatically select all the Hamlets from the 2000's and assign them to an
+// array called future_hamlets
+console.log("Scene V ---------------------------");
+
+// Act II
+
+var quotesArray = [
+    ["To be, or not to be: that is the question", "Hamlet-(Act III,Scene I)."],
+    ["A little more than kin, and less than kind", "Hamlet-(Act I,Scene II)."],
+    ["And it must follow, as the night the day, thou canst not then be false to any man", "Hamlet-(Act I,Scene III)."],
+    ["This is the very ecstasy of love", "Hamlet-(Act II,Scene I)."],
+    ["Brevity is the soul of wit", "Hamlet-(Act II,Scene II)."],
+    ["Do you think I am easier to be played on than a pipe?", "Hamlet-(Act III,Scene II)."],
+    ["Doubt that the sun doth move, doubt truth to be a liar, but never doubt I love", "Hamlet-(Act II,Scene II)."],
+    ["I will speak daggers to her, but use none", "Hamlet-(Act III,Scene II)."],
+    ["In my mind's eye", "Hamlet-(Act I,Scene II)."],
+    ["Neither a borrower nor a lender be; For loan oft loses both itself and friend, and borrowing dulls the edge of husbandry", "Hamlet-(Act I,Scene III)."],
+    ["Rich gifts wax poor when givers prove unkind", "Hamlet-(Act III,Scene I)."],
+    ["That it should come to this!", "Hamlet-(Act I,Scene II)."],
+    ["The lady doth protest too much, methinks", "Hamlet-(Act III,Scene II)."],
+    ["The plays the thing wherein I'll catch the conscience of the king", "Hamlet-(Act II,Scene II)."],
+    ["There is nothing either good or bad, but thinking makes it so", "Hamlet-(Act II,Scene II)."],
+    ["This above all: to thine own self be true", "Hamlet-(Act I,Scene III)."],
+    ["Though this be madness, yet there is method int.", "Hamlet-(Act II,Scene II)."],
+    ["What a piece of work is man! how noble in reason! how infinite in faculty! in form and moving how express and admirable! in action how like an angel! in apprehension how like a god! the beauty of the world, the paragon of animals! ", "Hamlet-(Act II,Scene II)."],
+    ["When sorrows come, they come not single spies, but in battalions", "Hamlet-(Act IV,Scene V)."]
+];
+
+// Scene I
+// Convert the following quotes array into an array of objects. Each object
+// should have the keys quote, play, act and scene
